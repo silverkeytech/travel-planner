cea0bff9ed78e2bfd71859b2b5b6c84e7e40950f
Rofayda <roufybassem@gamil.com>
2023-09-16 18:33:14 +0300

    Replacing testing data with variables

--- TravelPlanner/TravelPlanner/Pages/User/Reservation/AccommodationDetails.cshtml.cs
+++ TravelPlanner/TravelPlanner/Pages/User/Reservation/AccommodationDetails.cshtml.cs
@@ -1,12 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
+using TravelPlanner.Core.Accommodation;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TravelPlanner.Pages.User.Reservation
 {
     public class AccommodationDetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IAccommodationRepository _accommodationRepository;
+        public AccommodationView? Accommodation { get; set; }
+        public AccommodationDetailsModel(IAccommodationRepository accommodationRepository)
         {
+            _accommodationRepository = accommodationRepository;
+        }
+        public async Task<IActionResult> OnGet(Guid id)
+        {
+            Accommodation = await _accommodationRepository.GetAccommodationByIdAsync(id);
+            if (Accommodation == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
--- TravelPlanner/TravelPlanner/Pages/User/Reservation/AccommodationList.cshtml.cs
+++ TravelPlanner/TravelPlanner/Pages/User/Reservation/AccommodationList.cshtml.cs
@@ -1,12 +1,19 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TravelPlanner.Core.Accommodation;
 
 namespace TravelPlanner.Pages.User.Reservation
 {
     public class AccommodationListModel : PageModel
     {
-        public void OnGet()
+        private readonly IAccommodationRepository _accommodationRepository;
+        public List<AccommodationView> AccommodationList { get; set; } = new List<AccommodationView>();
+        public AccommodationListModel(IAccommodationRepository accommodationRepository)
         {
+            _accommodationRepository = accommodationRepository;
+        }
+        public async Task OnGet()
+        {
+            AccommodationList = await _accommodationRepository.GetAllAccommodationsAsync();
         }
     }
 }
