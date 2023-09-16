1c63218a210464671b1511f3d48c4eee91e9047d
Rofayda <roufybassem@gamil.com>
2023-09-16 18:35:23 +0300

    Updating places list

--- TravelPlanner/TravelPlanner/Pages/User/Reservation/PlacesList.cshtml.cs
+++ TravelPlanner/TravelPlanner/Pages/User/Reservation/PlacesList.cshtml.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TravelPlanner.Core.Place;
 
 namespace TravelPlanner.Pages.User.Reservation
 {
     public class PlacesListModel : PageModel
     {
-        public void OnGet()
+        private readonly IPlaceRepository _placeRepository;
+        public List<PlaceView> PlaceList { get; set; } = new List<PlaceView>();
+        public PlacesListModel(IPlaceRepository placeRepository)
         {
+            _placeRepository = placeRepository;
+        }
+        public async Task OnGet()
+        {
+            PlaceList = await _placeRepository.GetAllPlacesAsync();
         }
     }
 }
